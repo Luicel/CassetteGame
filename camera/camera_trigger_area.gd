a44ffc9f489c99cd961a0cee480f40fffbde9ba8
Luicel <luicelcs@gmail.com>
2023-11-14 20:04:46 -0600

    add camera trigger tool

--- camera/camera_trigger_area.gd
+++ camera/camera_trigger_area.gd
@@ -1,16 +1,36 @@
+@tool
 extends Area2D
 
 @export var used_phantom_camera_2d : PhantomCamera2D
+@export var camera_priority = 10
+
+@onready var camera_visibility_preview = $CameraVisibilityPreview
 
 
 func _ready():
-	connect("body_entered", _body_entered)
-	connect("body_exited", _body_exited)
+	if not Engine.is_editor_hint():
+		connect("body_entered", _body_entered)
+		connect("body_exited", _body_exited)
+		camera_visibility_preview.visible = false
+		camera_visibility_preview.disabled = true
+
+
+func _process(delta):
+	if Engine.is_editor_hint():
+		draw_camera_visibility_preview()
+
+
+func draw_camera_visibility_preview():
+	var rectangle_shape_2d = RectangleShape2D.new()
+	var width = ProjectSettings.get_setting("display/window/size/viewport_width")
+	var height = ProjectSettings.get_setting("display/window/size/viewport_height")
+	rectangle_shape_2d.size = Vector2(width * (1 / used_phantom_camera_2d.get_zoom().x), height * (1 / used_phantom_camera_2d.get_zoom().y))
+	camera_visibility_preview.shape = rectangle_shape_2d
 
 
 func _body_entered(body):
 	if body is CharacterBody2D:
-		used_phantom_camera_2d.set_priority(20)
+		used_phantom_camera_2d.set_priority(camera_priority)
 
 
 func _body_exited(body):
