d27bb4ab52559c4f50b996f711603a17633d0eb1
Luicel <luicelcs@gmail.com>
2023-11-25 17:16:44 -0600

    add camera bounds

---
+++ camera/camera_bounds.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var collision_shape_2d = $CollisionShape2D
+
+
+func _ready():
+	var camera = get_viewport().get_camera_2d()
+	
+	camera.limit_left = global_position.x - collision_shape_2d.shape.size.x / 2
+	camera.limit_right = global_position.x + collision_shape_2d.shape.size.x / 2
+	camera.limit_top = collision_shape_2d.global_position.y - collision_shape_2d.shape.size.y / 2
+	camera.limit_bottom = collision_shape_2d.global_position.y + collision_shape_2d.shape.size.y / 2
