bee86edfc4258efbbdffad503e5ec1852b31f73d
Luicel <luicelcs@gmail.com>
2023-12-01 16:26:57 -0600

    add anti red tiles

--- cassette/red/red_cassette.gd
+++ cassette/red/red_cassette.gd
@@ -3,9 +3,9 @@ extends BaseCassette
 
 func _enable_effect():
 	get_tree().get_first_node_in_group("player").transition_to_movement_state("limited")
-	get_tree().call_group("red_block", "enable")
+	get_tree().call_group("red_tile", "toggle")
 
 
 func _disable_effect():
 	get_tree().get_first_node_in_group("player").transition_to_movement_state("normal")
-	get_tree().call_group("red_block", "disable")
+	get_tree().call_group("red_tile", "toggle")
--- level/obstacles/red_tile/red_tile.gd
+++ level/obstacles/red_tile/red_tile.gd
@@ -1,18 +1,37 @@
 extends StaticBody2D
 
+@export var start_active = false
+@export var active_texture : Texture2D
+@export var inactive_texture : Texture2D
+
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var collision_shape_2d = $CollisionShape2D
+@onready var sprite_2d = $Sprite2D
 @onready var player_detector_area = $PlayerDetectorArea
 
+var is_active = false
 var is_player_in_area = false
 var is_trying_to_activate = true
 
 func _ready():
-	disable()
+	is_active = start_active
+	
+	if not start_active:
+		disable()
+
+
+func toggle():
+	if is_active:
+		disable()
+	else:
+		enable()
 
 
 func enable():
+	is_active = true
+	
 	if not is_player_in_area:
+		sprite_2d.texture = active_texture
 		visible = true
 		collision_shape_2d.set_deferred("disabled", false)
 	else:
@@ -20,9 +39,13 @@ func enable():
 
 
 func disable():
+	is_active = false
+	
 	is_trying_to_activate = false
+	sprite_2d.texture = inactive_texture
 	visible = false
 	collision_shape_2d.set_deferred("disabled", true)
+	
 
 
 func _on_player_detector_area_body_entered(body):
